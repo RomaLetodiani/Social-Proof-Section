8ba20287c048e998069f74df8e69ee65a9d851e6
RomaLetodiani <roman.letodiani03@gmail.com>
2023-09-23 17:58:02 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,75 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="content">
+      <div className="wrapper">
+        <header>
+          <div>
+            <h1>10,000+ of our users love our products.</h1>
+            <p>
+              We only provide great products combined with excellent customer
+              service. See what our satisfied customers are saying about our
+              services.
+            </p>
+          </div>
+          <div>
+            <div>
+              stars <p>Rated 5 Stars in Reviews</p>
+            </div>
+            <div>
+              stars <p>Rated 5 Stars in Report Guru</p>
+            </div>
+            <div>
+              stars <p>Rated 5 Stars in BestTech</p>
+            </div>
+          </div>
+        </header>
+        <footer>
+          <div>
+            <div>
+              <img src="/images/image-colton.jpg" alt="anne" />
+              <div>
+                <h2>Colton Smith</h2>
+                <h3>Verified Buyer</h3>
+              </div>
+            </div>
+            <p>
+              "We needed the same printed design as the one we had ordered a
+              week prior. Not only did they find the original order, but we also
+              received it in time. Excellent!"
+            </p>
+          </div>
+          <div>
+            <div>
+              <img src="/images/image-irene.jpg" alt="anne" />
+              <div>
+                <h2>Irene Roberts</h2>
+                <h3>Verified Buyer</h3>
+              </div>
+            </div>
+            <p>
+              "Customer service is always excellent and very quick turn around.
+              Completely delighted with the simplicity of the purchase and the
+              speed of delivery."
+            </p>
+          </div>
+          <div>
+            <div>
+              <img src="/images/image-anne.jpg" alt="anne" />
+              <div>
+                <h2>Anne Wallace</h2>
+                <h3>Verified Buyer</h3>
+              </div>
+            </div>
+            <p>
+              "Put an order with this company and can only praise them for the
+              very high standard. Will definitely use them again and recommend
+              them to everyone!"
+            </p>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
